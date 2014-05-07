479635e0ae85c1d02f727c2d04e6a31350a1466d
samdmarshall <me@samdmarshall.com>
2014-05-06 21:10:42 -0400

    it builds

---
+++ libsecurity_utilities/lib/security_utilities.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2003-2004 Apple Computer, Inc. All Rights Reserved.
+ * 
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this
+ * file.
+ * 
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ * 
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
+#ifndef _SECURITY_UTILITIES_H_
+#define _SECURITY_UTILITIES_H_
+
+#if defined(__cplusplus)
+
+#include "bufferfifo.h"
+#include "buffers.h"
+#include "cfutilities.h"
+#include "daemon.h"
+#include "debugging.h"
+#include "debugsupport.h"
+#include "devrandom.h"
+#include "endian.h"
+#include "fdmover.h"
+#include "fdsel.h"
+#include "globalizer.h"
+#include "headermap.h"
+#include "hosts.h"
+#include "inetreply.h"
+#include "ip++.h"
+#include "logging.h"
+#include "mach_notify.h"
+#include "memstreams.h"
+#include "memutils.h"
+#include "../../libsecurity_utilities/lib/refcount.h"
+#include "seccfobject.h"
+#include "selector.h"
+#include "socks++.h"
+#include "socks++4.h"
+#include "socks++5.h"
+#include "streams.h"
+#include "threading.h"
+#include "threading_internal.h"
+#include "timeflow.h"
+#include "tqueue.h"
+#include "typedvalue.h"
+#include "unix++.h"
+#include "url.h"
+#include "utilities.h"
+#include "utility_config.h"
+
+#endif /* defined(__cplusplus) */
+
+#endif /* _SECURITY_UTILITIES_H_ */
